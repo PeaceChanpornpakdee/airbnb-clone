52c84582968981ee2afda4a5c6caa4dad2de0c7e
PeaceChanpornpakdee <yothinpeace@gmail.com>
2024-10-16 19:09:40 +0700

    ENH: Move Logo to Left Side

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,19 +7,18 @@ const Header = () => {
   return (
     <header className="bg-white shadow-md py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <div className="text-red-500 text-2xl font-bold">Airbnb Clone</div>
+        <a href="/">
+          {/* Show the first logo on small screens */}
+          <div className="block lg:hidden">
+            <AirbnbLogoIcon className="airbnb-logo" />
+          </div>
+          {/* Show the second logo on larger screens */}
+          <div className="hidden lg:block">
+            <AirbnbLogoIconWithText className="airbnb-logo" />
+          </div>
+        </a>
         <nav>
           <ul className="flex space-x-6">
-            <a href="/">
-              {/* Show the first logo on small screens */}
-              <div className="block lg:hidden">
-                <AirbnbLogoIcon className="airbnb-logo" />
-              </div>
-              {/* Show the second logo on larger screens */}
-              <div className="hidden lg:block">
-                <AirbnbLogoIconWithText className="airbnb-logo" />
-              </div>
-            </a>
             <li className="text-gray-700">Become a Host</li>
             <li className="text-gray-700">Help</li>
             <li className="text-gray-700">Sign In</li>
