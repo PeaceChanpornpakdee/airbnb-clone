ee2ff14d25ff199c83ac2de354e00b4fe928682f
PeaceChanpornpakdee <yothinpeace@gmail.com>
2024-11-01 18:37:48 +0700

    ENH: Working Overflow with Flex (Test)

--- src/components/Overflow.tsx
+++ src/components/Overflow.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Overflow: React.FC = () => {
   return (
-    <div className="w-full bg-black h-10 overflow-x-auto">
-      <div className="bg-red-500 w-[1000px] h-6"></div>
+    <div className="w-full bg-black h-10 flex">
+      <div className="bg-green-500 w-full h-6 shrink overflow-x-auto">
+        <div className="bg-red-500 w-[1000px] h-4"></div>
+      </div>
+      <div className="bg-blue-500 w-[100px] h-6 shrink-0"></div>
     </div>
   );
 };
