b8e0cafa569c227f582d2ca73a049d62e4c3b2a6
PeaceChanpornpakdee <yothinpeace@gmail.com>
2024-11-01 14:04:10 +0700

    ENH: Clone Shevron Button

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import Header from "./components/Header";
 import Menubar from "./components/Menubar";
 // import Hero from "./components/Hero";
+// import ShrinkingHeader from "./components/ShrinkingHeader";
 import Listings from "./components/Listings";
+import ChevronButton from "./components/ChevronButton";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Menubar />
+      {/* <ShrinkingHeader /> */}
       {/* <Hero /> */}
+      <ChevronButton onClick={() => console.log("Hi")} />
       <Listings />
     </div>
   );
